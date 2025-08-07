43c0c296de421da3f47942db0c0df962a434afc9
lucas <nemokyto@gmail.com>
2025-08-07 12:28:39 -0300

    adicionado: arquivo de configurações do tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/ @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app//.{js,ts,jsx,tsx}", 
+    "./components/**/.{js,ts,jsx,tsx}", 
+    "./src/*/.{js,ts,jsx,tsx}", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+postcss.config.mjs
+const config = { plugins: { "@tailwindcss/postcss": {} } };
+export default config;
